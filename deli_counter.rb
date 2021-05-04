9b719926e6e1fb54a699a38810408a2fbb55d0e2
Lj <tnedutsl457892@gmail.com>
2021-05-04 00:14:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,14 +14,15 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  end
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli.length > 0
     puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
-  else
+  else 
     puts "There is nobody waiting to be served!"
   end
 end
+
